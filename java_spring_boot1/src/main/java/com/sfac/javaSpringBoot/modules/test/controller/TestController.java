647ee4a97396fd899f16f24186e11f65bf897503
liguoliang <2313458226@qq.com>
2020-08-10 14:19:08 +0800

    "这是一个spring_boot项目测试"

---
+++ java_spring_boot1/src/main/java/com/sfac/javaSpringBoot/modules/test/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.sfac.javaSpringBoot.modules.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/testDese")
+    @ResponseBody
+    public String testDese(){
+        return "这是一个很牛的软件。";
+    }
+}
