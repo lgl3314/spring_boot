cd203c26464b8930842a4879ca410f1964878018
liguoliang <2313458226@qq.com>
2020-08-12 09:12:43 +0800

    "2020/8/11"

--- java_spring_boot/src/main/java/com/sfac/javaSpringBoot/JavaSpringBootApplication.java
+++ java_spring_boot/src/main/java/com/sfac/javaSpringBoot/JavaSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.hqyj.javaSpringBoot;
+package com.sfac.javaSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- java_spring_boot/src/test/java/com/sfac/javaSpringBoot/JavaSpringBootApplicationTests.java
+++ java_spring_boot/src/test/java/com/sfac/javaSpringBoot/JavaSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hqyj.javaSpringBoot;
+package com.sfac.javaSpringBoot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
